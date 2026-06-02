e03c489d2fcc25e0fc6c5e2326739d1e0b2af972
the brownie <spiritdude484@gmail.com>
2026-06-02 14:50:55 +0100

    Update chatdetection.lua

--- chatdetection.lua
+++ chatdetection.lua
@@ -1,6 +1,6 @@
 game.Players.PlayerAdded:Connect(function(player)
 	player.Chatted:Connect(function(msg)
-		if msg == "yourmessagehere" then
+		if msg == "yourmessagehere" then -- Put what the player says here in the speech marks.
 			-- Your function here
 		end
 	end)
